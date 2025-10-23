fa699e1b12034c52b4d873b74bcf6513cc238134
hellojunho <jo62698476@gmail.com>
2025-10-23 15:37:26 +0900

    구조 변경

---
+++ doit/search(dfs, bfs)/baekjoon/inequality_sign.py
@@ -0,0 +1,18 @@
+import sys
+
+input = sys.stdin.readline
+
+k = int(input())
+sign = map(str, input().split())
+
+nums = [i for i in range(10)]
+
+# print(nums)
+
+
+
+# def dfs(start):
+
+
+#     for i in range(len(nums)):
+        
